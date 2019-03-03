ca96a3c530f94fe97858c8ba30e09e343b10c40a
Mateusz Frankowski <mf385442@students.mimuw.edu.pl>
2019-03-03 16:45:37 +0100

    Update oisc.cc

--- oisc.cc
+++ oisc.cc
@@ -6,55 +6,60 @@ string input, x, a, b, v, help;
 vector <string> q, results;
 int res, a1, b1, aminus, bminus, c, last_res, pos;
 int toInt (string x) {
-  int res;
-	istringstream iss(x);
-  iss >> res;
-  return res;
+	int res;
+   istringstream iss(x);
+   iss >> res;
+   return res;
 }
 string toString (int x) {
-  ostringstream temp;
-  temp<<x;
-  string res=temp.str();
-	return res;
+  	ostringstream temp;
+  	temp<<x;
+  	string res=temp.str();
+  	return res;
 }
 void newSubleq (string a, string b, string c) {
-  results.push_back(a + " " + b + " " + c);
+  	results.push_back(a + " " + b + " " + c);
 }
-int main()
-{
-	ios_base::sync_with_stdio(0);
+int main() {
 
-	while (getline(cin,input))
-	{
-    results.clear();
+	ios_base::sync_with_stdio(0);
+	
+	while (getline(cin,input)) {
+	
+   	results.clear();
 		res = 9001;
 		newSubleq(toString(res), toString(res), " 12");
 		q.clear();
 		newSubleq("0", "0", "15");
 		c=18;
 		pos=0;
-		while (1)
-		{
-      if (pos >= input.size()) break;
+		
+		while (1) {
+      	if (pos >= input.size()) {
+      		break;
+      	}
 			x = "";
-			if (input[pos]==' ') pos++;
-			for (; pos<input.size(); pos++)
-			{
+			if (input[pos]==' ') {
+				pos++;
+			}
+			for (; pos<input.size(); pos++) {
 				if (input[pos] == ' ') break;
 				x += input[pos];
 			}
 
-			if (x != "+" && x != "-" && x != "*" && x != "/") q.push_back(x);
-			else
-			{
+			if (x != "+" && x != "-" && x != "*" && x != "/") {
+				q.push_back(x);
+			}
+			else {
 				b = q.back();
 				q.pop_back();
 				a = q.back();
 				q.pop_back();
-				if (a[0] == 'M')
-				{
+				if (a[0] == 'M') {
 					help = "";
-					for (int i = 1; i < a.size(); i++) help += a[i];
+					for (int i = 1; i < a.size(); i++) {
+						help += a[i];
+					}
 					a1 = toInt(help);
 					aminus = res;
 					res++;
@@ -63,8 +68,7 @@ int main()
 					newSubleq(toString(a1), toString(aminus), toString(c));
 					c += 3;
 				}
-				else
-				{
+				else {
 					a1 = res;
 					res++;
 					newSubleq(toString(res), toString(res), toString(c));
@@ -79,10 +83,11 @@ int main()
 					c += 3;
 				}
 
-				if (b[0] == 'M')
-				{
+				if (b[0] == 'M') {
 					help = "";
-					for (int i = 1; i < b.size(); i++) help += b[i];
+					for (int i = 1; i < b.size(); i++) {
+						help += b[i];
+					}
 					b1 = toInt(help);
 					bminus = res;
 					res++;
@@ -91,8 +96,7 @@ int main()
 					newSubleq(toString(b1), toString(bminus), toString(c));
 					c += 3;
 				}
-				else
-				{
+				else {
 					b1 = res;
 					res++;
 					newSubleq(toString(res), toString(res), toString(c));
@@ -107,8 +111,7 @@ int main()
 					c += 3;
 				}
 
-				if (x == "-")
-				{
+				if (x == "-") {
 					newSubleq(toString(a1), toString(b1), toString(c));
 					c += 3;
 					newSubleq(toString(b1), toString(res), toString(c));
@@ -120,8 +123,7 @@ int main()
 					v = "M" + toString(last_res);
 					q.push_back(v);
 				}
-				else if (x == "+")
-				{
+				else if (x == "+") {
 					newSubleq(toString(b1), toString(aminus), toString(c));
 					c += 3;
 					newSubleq(toString(aminus), toString(res), toString(c));
@@ -133,8 +135,7 @@ int main()
 					v = "M" + toString(last_res);
 					q.push_back(v);
 				}
-				else if (x == "*")
-				{
+				else if (x == "*") {
 					newSubleq("-1", toString(aminus), toString(c));
 					c += 6;
 					newSubleq(toString(bminus), toString(res), toString(c));
@@ -147,8 +148,7 @@ int main()
 					v = "M" + toString(last_res);
 					q.push_back(v);
 				}
-				else if (x == "/")
-				{
+				else if (x == "/") {
 					newSubleq(toString(bminus), toString(aminus), toString(c));
 					c += 6;
 					newSubleq("-1", toString(res), toString(c));
@@ -164,13 +164,11 @@ int main()
 			}
 		}
 
-
-		if (q[0][0] == 'M')
-		{
+		if (q[0][0] == 'M') {
 			cout << results.size() + 3 << "\n";
 			for (int i = 0; i < results.size(); i++) {
-        cout << results[i] << "\n";
-      }
+        		cout << results[i] << "\n";
+      	}
 			cout << last_res << " " << res << " " << c << "\n";
 			c += 3;
 			cout << res << " " << 0 << " " << c <<"\n";
@@ -178,10 +176,11 @@ int main()
 			res++;
 			cout << c << " " << c << " " << 10000 <<"\n";
 		}
-		else
-		{
+		else {
 			cout << results.size() + 2 << "\n";
-			for (int i = 0; i < results.size(); i++) cout << results[i] << "\n";
+			for (int i = 0; i < results.size(); i++) {
+				cout << results[i] << "\n";
+			}
 			cout << "-" << q[0] << " " << 0 << " " << c << "\n";
 			c += 3;
 			res++;
